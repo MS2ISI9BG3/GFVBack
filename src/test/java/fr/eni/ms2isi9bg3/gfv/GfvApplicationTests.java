460ea4199aa40a810b742e6a50610081a3f506fa
dndizeye <donald.ndizeye@gmail.com>
2020-03-01 19:02:10 +0100

    Initial commit

---
+++ src/test/java/fr/eni/ms2isi9bg3/gfv/GfvApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.ms2isi9bg3.gfv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
